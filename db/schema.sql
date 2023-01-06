c334e57ceacc489515811c5d82e98c4769383e96
hmustain <hunter@bunkerbranding.com>
2023-01-06 11:30:05 -0600

    added table for employees

--- db/schema.sql
+++ db/schema.sql
@@ -15,9 +15,22 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(6,2) NOT NULL,
-    department_id INT,
+    department_id INT NOT NULL
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
     ON DELETE SET NULL
+);
+
+-- CREATE TABLE FOR EMPLOYEES
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT INT NOT NULL
+    manager_id INT 
+    REFERENCES employees(id)
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
 );
 
