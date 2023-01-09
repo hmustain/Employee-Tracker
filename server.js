c15adc5cff2271f4d35bb8105b24c5ac75cee554
hmustain <hunter@bunkerbranding.com>
2023-01-09 08:56:22 -0600

    connected db and added queries

--- server.js
+++ server.js
@@ -2,6 +2,52 @@
 const inquirer = require(`inquirer`);
 const fs = require(`fs`);
 
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'company_db'
+  },
+  console.log(`Connected to the courses_db database.`)
+);
+
+// Query database
+db.query('SELECT * FROM departments', function (err, results) {
+    console.table(results);
+  });
+
+  db.query('SELECT title, salary, FROM roles, departments.name as department_name FROM company_db LEFT JOIN departments ON roles.departments=departments.id; ', function (err, results) {
+    console.table(results);
+  });
+
+  db.query('SELECT * FROM employees', function (err, results) {
+    console.table(results);
+  });
+  
+  // Default response for any other request (Not Found)
+  app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+
 function menu() {
     inquirer
         .prompt([
